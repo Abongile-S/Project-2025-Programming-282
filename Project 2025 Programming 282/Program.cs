4e585f8d1c6b0b4b72f9ed9a621e128b9d4866f1
Mzingeli Ncube <601727@student.belgiumcampus.ac.za>
2025-10-21 19:21:43 +0200

    hero form change

--- Project 2025 Programming 282/Program.cs
+++ Project 2025 Programming 282/Program.cs
@@ -37,7 +37,7 @@ namespace Project_2025_Programming_282
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1(heroFile, summaryFile));
+            Application.Run(new Form1(heroFile, summaryFile));aw
         }
     }
 }
